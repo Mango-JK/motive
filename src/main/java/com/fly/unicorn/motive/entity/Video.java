76915c6b65f7b7b1ca29ab2db27aa1d2101fb738
jeongkeon <vonjobi2@gmail.com>
2022-05-22 10:42:26 +0900

    [refactor] set maria db

--- src/main/java/com/fly/unicorn/motive/entity/Video.java
+++ src/main/java/com/fly/unicorn/motive/entity/Video.java
@@ -1,57 +1,57 @@
-package com.fly.unicorn.motive.entity;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.*;
-import org.springframework.web.multipart.MultipartFile;
-
-import javax.persistence.*;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotBlank;
-import java.time.LocalDateTime;
-
-@Getter
-@ToString
-@NoArgsConstructor
-@AllArgsConstructor
-@Entity
-public class Video extends BaseTimeEntity {
-	@JsonProperty(value = "VIDEO_ID")
-	@Column(name = "video_id")
-	@Min(value = 1)
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Id
-	private long videoId;
-
-	// Todo User
-
-	@JsonProperty(value = "DAY")
-	@Column(name = "day", columnDefinition = "TIMESTAMP")
-	private LocalDateTime day;
-
-	@JsonProperty(value = "TITLE")
-	@Column(name = "title")
-	@NotBlank
-	private String title;
-
-	@NotBlank
-	@JsonProperty(value = "URL")
-	@Column(name = "url")
-	@Lob
-	private String url;
-
-	@JsonProperty(value = "THUMBNAIL")
-	@Column(name = "thumbnail")
-	@Lob
-	private MultipartFile thumbnail;
-
-	@JsonProperty(value = "DESCRIPTION")
-	@Column(name = "description")
-	@Lob
-	private String description;
-
-	@JsonProperty(value = "RUNNING_TIME")
-	@Column(name = "runningTime")
-	private int runningTime;
-
-	// Todo Comment
-}
+//package com.fly.unicorn.motive.entity;
+//
+//import com.fasterxml.jackson.annotation.JsonProperty;
+//import lombok.*;
+//import org.springframework.web.multipart.MultipartFile;
+//
+//import javax.persistence.*;
+//import javax.validation.constraints.Min;
+//import javax.validation.constraints.NotBlank;
+//import java.time.LocalDateTime;
+//
+//@Getter
+//@ToString
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@Entity
+//public class Video extends BaseTimeEntity {
+//	@JsonProperty(value = "VIDEO_ID")
+//	@Column(name = "video_id")
+//	@Min(value = 1)
+//	@GeneratedValue(strategy = GenerationType.IDENTITY)
+//	@Id
+//	private long videoId;
+//
+//	// Todo User
+//
+//	@JsonProperty(value = "DAY")
+//	@Column(name = "day", columnDefinition = "TIMESTAMP")
+//	private LocalDateTime day;
+//
+//	@JsonProperty(value = "TITLE")
+//	@Column(name = "title")
+//	@NotBlank
+//	private String title;
+//
+//	@NotBlank
+//	@JsonProperty(value = "URL")
+//	@Column(name = "url")
+//	@Lob
+//	private String url;
+//
+//	@JsonProperty(value = "THUMBNAIL")
+//	@Column(name = "thumbnail")
+//	@Lob
+//	private MultipartFile thumbnail;
+//
+//	@JsonProperty(value = "DESCRIPTION")
+//	@Column(name = "description")
+//	@Lob
+//	private String description;
+//
+//	@JsonProperty(value = "RUNNING_TIME")
+//	@Column(name = "runningTime")
+//	private int runningTime;
+//
+//	// Todo Comment
+//}
--- src/test/java/com/fly/unicorn/motive/service/WordingServiceTest.java
+++ src/test/java/com/fly/unicorn/motive/service/WordingServiceTest.java
@@ -1,15 +1,31 @@
 package com.fly.unicorn.motive.service;
 
+import com.fly.unicorn.motive.entity.Wording;
+import com.fly.unicorn.motive.repository.WordingRepository;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
-@ExtendWith(SpringExtension.class)
+import java.time.LocalDateTime;
+
+@SpringBootTest
 class WordingServiceTest {
+
+	@Autowired
+	WordingRepository wordingRepository;
 
 	@Test
 	public void postTest() {
 		System.out.println("SUCCESS");
+		Wording wording = Wording.builder()
+			.day(LocalDateTime.now())
+			.description("test")
+			.word("good")
+			.writer("me")
+			.build();
+
+		wordingRepository.save(wording);
+		System.out.println("finish");
 	}
 
 }
